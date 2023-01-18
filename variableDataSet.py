3cfc8d82b4d57225f5d9ca4367f3ee285ca91f99
MirzaHilmi <exquisitemirza@student.ub.ac.id>
2023-01-19 00:28:26 +0700

    utilize a float class to represent decimal number

--- variableDataSet.py
+++ variableDataSet.py
@@ -3,6 +3,11 @@
 # last_name = "Hilmi"
 # print(first_name + ' ' + last_name)
 
-number = 67
-number += 1
-print("the number is: " + str(number))
+# number = 67
+# number += 1
+# print("the number is: " + str(number))
+
+score = 68.9
+print(score)
+print(type(score))
+print("the decimal number is: " + str(score))
