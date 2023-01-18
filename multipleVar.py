02dd961d299301014552ffbe0cafc64d09db772a
MirzaHilmi <exquisitemirza@student.ub.ac.id>
2023-01-19 00:49:15 +0700

    assign multiple variable with the same data type in one line

--- multipleVar.py
+++ multipleVar.py
@@ -2,3 +2,7 @@ name, age, height, martian = "Mirza", 37, 310, False
 print("Name: " + name + '\nAge: ' + str(age)
       + '\nHeight: ' + str(height) + '\nIs a Martian: '
       + str(martian))
+
+first_problem = second_problem = third_problem = last_problem = 9
+print(first_problem + second_problem + third_problem + last_problem)
+
