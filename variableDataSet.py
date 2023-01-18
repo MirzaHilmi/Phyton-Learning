43ce5a504ba44d26c553210818ccf7fc78960e4c
MirzaHilmi <exquisitemirza@student.ub.ac.id>
2023-01-19 00:21:03 +0700

    modified

--- variableDataSet.py
+++ variableDataSet.py
@@ -1,4 +1,8 @@
 # print("Hello")
-first_name = "Mirza"
-last_name = "Hilmi"
-print(first_name + ' ' + last_name)
+# first_name = "Mirza"
+# last_name = "Hilmi"
+# print(first_name + ' ' + last_name)
+
+number = 67
+number += 1
+print(type(number))
